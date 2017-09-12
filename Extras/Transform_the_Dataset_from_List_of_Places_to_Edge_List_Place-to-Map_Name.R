43fafc61939689614714db9b658510689931b03e
Jacob Levernier <jlevern@upenn.edu>
2017-09-12 09:49:19 -0400

    Added files to the repo, after testing pre-commit hook.

---
+++ Extras/Transform_the_Dataset_from_List_of_Places_to_Edge_List_Place-to-Map_Name.R
@@ -0,0 +1,120 @@
+# Script for taking comma-separated strings (within cells of a CSV) and combining them into one-way relationships for easy import into a network mapping application
+# Jacob Levernier
+# September 2017
+# Version: 0.1.0
+# Released under a BSD 3-Clause License (https://opensource.org/licenses/BSD-3-Clause).
+
+# Clear R's memory, setting a fresh foundation from which to run this script.
+rm(list=ls())
+
+#################
+# CONFIGURATION SETTINGS -- EDIT THESE
+#################
+
+# Working directory: The directory on your computer in which the data file can be found, and into which you would like to save the output from this script. Include a trailing slash ('/') at the end:
+
+# If you don't know what a directory path is: In RStudio, you can click on Session -> Set Working Directory... -> Choose Directory..., then select the directory that contains Mappamundi_Project_Full_Dataset_Edge_List.csv. You can then take the path that's printed at the bottom of the window, and paste that between the quotation marks here:
+workingDirectoryToSet <- "/path/to/directory/that/contains/Mappamundi_Project_Full_Dataset_Edge_List.csv"
+
+# Data file name within the working directory:
+# NOTE: The data file should be a Comma-Separated Values file with comma delimiters and double-quotes (") around text fields.
+
+# The CSV file SHOULD have a header row (i.e., a row with column names).
+dataFileName <- "2017-08-27-1404_Mappamundi_Project_Full_Dataset.csv"
+transpose_dataset_first <- TRUE # This should be TRUE or FALSE. It should be TRUE if the data are arranged with each map as a separate column. It should be FALSE if the data are arranged with each map as a separate row.
+
+# The filename to which to write the output from this script:
+fileNameToWriteOutputTo <- "2017-08-27-1404_Mappamundi_Project_Full_Dataset_Edge_List.csv"
+
+filename_to_write_unique_names_to_check_to <- "Unique_Values_to_Look_Over_for_Duplicates.txt"
+
+#################
+# END CONFIGURATION SETTINGS
+#################
+
+
+
+#################
+# PROCESSING STEPS -- DO NOT EDIT THESE
+#################
+
+# Set the working directory
+setwd(workingDirectoryToSet)
+
+dataSetToParse <- read.csv(dataFileName, header = TRUE, sep=",", stringsAsFactors = FALSE)
+
+# If we've been asked to transpose the dataset, do so:
+if(transpose_dataset_first == TRUE){
+	dataSetToParse <- as.data.frame(t(dataSetToParse))
+	dataSetToParse <- cbind(rownames(dataSetToParse), dataSetToParse)
+
+	# Remove the first row, which would have been row titles in the originally-transposed dataset:
+	dataSetToParse <- dataSetToParse[2:nrow(dataSetToParse),]
+}
+
+# Create a blank data.frame, which we'll append to below:
+edge_list_of_places_and_map_names <- data.frame()
+
+for(i in 1:nrow(dataSetToParse)){
+	message("Processing row ", i, "...")
+
+		vector_of_places_in_row <- unlist(dataSetToParse[i, 2:ncol(dataSetToParse)])
+
+		map_name <- dataSetToParse[i, 1]
+
+		vector_of_places_in_row_with_blanks_removed <- vector_of_places_in_row[vector_of_places_in_row != ""]
+
+		pairwise_combinations_of_places <- data.frame(
+			"from" = vector_of_places_in_row_with_blanks_removed,
+			"to" = map_name
+		)
+
+	# Check our work
+	# View(pairwise_combinations_of_places)
+
+	edge_list_of_places_and_map_names <- rbind(edge_list_of_places_and_map_names, pairwise_combinations_of_places)
+} # End of for loop
+
+# Lowercase everything:
+edge_list_with_lowercased_rows <- apply(
+	edge_list_of_places_and_map_names,
+	2,
+	tolower
+)
+# View(edge_list_with_lowercased_rows) # Check our work
+
+
+# Write a CSV file for this:
+write.table(
+	edge_list_with_lowercased_rows,
+	file = fileNameToWriteOutputTo,
+	row.names = FALSE,
+	col.names = TRUE,
+	sep=",",
+	eol="\r\n" # The eol/"end of line" marker \r\n is \n, which is read by older Mac machines as "new line", \r, which is read by Unix machines as newline (or "carriage Return"), and \r\n, which is read by Windows machines as newline (see, e.g., https://stackoverflow.com/questions/1761051/difference-between-n-and-r/1761086#1761086)
+)
+
+
+edge_list_with_lowercased_rows.data_frame <- as.data.frame(edge_list_with_lowercased_rows)
+
+unique_values <- as.character(
+	sort(unique(
+		edge_list_with_lowercased_rows.data_frame[
+			grep("Normalized_Reading", edge_list_with_lowercased_rows.data_frame$to, ignore.case = TRUE) # Just use rows from a particular set of maps (ones whose names match the search string here).
+			,
+			"from"
+			]
+	))
+)
+
+write.table(
+	unique_values,
+	file = filename_to_write_unique_names_to_check_to,
+	row.names = TRUE,
+	quote = FALSE
+)
+
+
+#################
+# END PROCESSING STEPS
+#################
---
+++ README.md
@@ -0,0 +1,57 @@
+# The Psalter Maps, and other medieval mappamundi, transcribed and normalized
+
+## Current maintainers
+
+This dataset and respository is currently maintained by Heather G. Wacha <wacha2@wisc.edu> and Jacob G. Levernier <jlevern@upenn.edu>.
+
+## Contributors
+
+<!--
+Initial contributors:
+-->
+
+- Heather G. Wacha: medieval domain expert who compiled and normalized the data
+- Jacob G. Levernier: provided data management and data schema advice, and managed data pre-processing
+
+## Contents
+
+
+
+## Using this dataset
+
+### Conditions of use
+
+Please see the `LICENSE.txt` file for license information (i.e., the conditions under which you may reuse this dataset). In summary, we have released this dataset for others (especially researchers) to use freely, *provided the dataset is cited (see below) and any changes to it are shared back with the research community (preferably through this repository).*
+
+Please see the `CITATION.txt` file for an example citation to use for this datset. That file also contains information on how to refer to different "versions" of the dataset going forward.
+
+### Understanding and processing the dataset
+
+After the first column, each column of the dataset represents one reading of one map. Currently, each map is represented across two columns. Rows are unimportant in the dataset, except that, within each map, a row contains variations on the same toponym from that map, across its two columns in the datset:
+
+1. The **`Diplomatic_Reading`** column contains an exact transcription of the contents of the map.
+1. The **`Normalized_Reading`** column contains a normalized version of the `Diplomatic_Reading` column. See [below](#our-approach-to-normalizing "Our approach to normalizing") for detail on our approach to normalizing these data.
+
+The `Extras` directory contains an example script written in R for transforming the dataset into a toponym-to-map "edge list" / ["adjacency list"](https://en.wikipedia.org/wiki/Adjacency_list "Wikipedia: 'Adjacency List'"), to facilitate network analyses of these data. This script also facilitates creating an alphabetized text file of all unique toponyms across all maps, to facilitate deduplication (e.g., looking for mis-spelled, etc. toponyms).
+
+### Our approach to normalizing
+
+\[We will be adding this soon.]
+
+### Contributing to this dataset
+
+## Contributing to this Repository
+
+We welcome contributions to this dataset!
+
+**We also welcome contributions in documentation** -- if there are things that you had to work to understand (for example, how to even download these files in the first place), we welcome additional text for a Wiki that we can attach to this repository.
+
+If you would like to contribute to this repository, please follow these steps:
+
+1. 
+  1. If you feel comfortable using Git:
+    1. Make a fork of this repository (from this repository's page on GitHub, click "Fork").
+    1. Create a single commit that contains all of the changes that you would like to share (either by "squashing" your commits (see [here](http://stackoverflow.com/a/5189600 'StackOverflow: Squash my last X commits together using Git'), for example), or by just copying your altered files into a fresh copy of this repository).
+    1. Make a pull request through GitHub (from your copy of the repository in GitHub, click "New Pull Request").
+  1. If you do not feel comfortable using Git: Send an email to the current repository maintainers listed [above](#current-maintainers 'Current maintainers'); we will work together to get your changes incorporated, and will add you to the [list of contributors](#contributors "List of contributors") in this Readme. We will also add you (for any affected versions of the dataset) to the example citation in the `CITATION.txt` file.
+
